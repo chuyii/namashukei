a35800a986a43dfdb2e9d0e548f0f0fc2e26c034
chuyii <contact@chuyii.net>
2023-11-25 15:41:05 +0900

    TanStack Query 周りを追加

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,9 @@
+import React from 'react';
+
 import type { Preview } from '@storybook/react';
+
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '../src/lib/query';
 
 import '../src/app/globals.css';
 
@@ -12,6 +17,11 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (story) => (
+      <QueryClientProvider client={queryClient}>{story()}</QueryClientProvider>
+    ),
+  ],
 };
 
 export default preview;
